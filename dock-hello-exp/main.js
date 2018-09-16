0c78b763edf7e593e2596f7631fd37d40936351f
hanubindh <h.b.krishna@gmail.com>
2018-09-16 23:58:31 +0530

    Updated main.js

--- dock-hello-exp/main.js
+++ dock-hello-exp/main.js
@@ -1,5 +1,25 @@
 const express = require("express");
+const os = require("os");
 
 const host = "0.0.0.0"
 const port = 8080;
+const app = express();
+const idLength = 10000;
 
+
+
+app.get("/", (req, resp) => {
+	// The Laziest way to create an object "literally" :p
+	var data = {
+		id: (Math.floor((Math.random() + 1) * 10000)).toString(),
+		time: new Date().toJSON(),
+		hostName: os.hostname(),
+		message: "Hello World"
+	}; 
+	console.log(`Tx-${data.id} : Responding to request`);
+	resp.send(data);
+});
+
+app.listen(port, host, () => {
+	console.log(`App started in ${host}:${port}`);
+});
